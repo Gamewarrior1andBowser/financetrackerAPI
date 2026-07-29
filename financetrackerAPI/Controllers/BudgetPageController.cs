eaa12c76d7681d918fcb559cd6df29c6b2bd574c
Harpreet <ksfibri@gmail.com>
2026-07-29 12:23:09 -0500

    Create budget page and display budget data

---
+++ financetrackerAPI/Controllers/BudgetPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace financetrackerAPI.Controllers
+{
+    public class BudgetPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Budget/Index.cshtml");
+        }
+    }
+}
