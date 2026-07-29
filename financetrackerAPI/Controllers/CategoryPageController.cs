70faeedf25ea5c4831c5ca2e120676dc0da1534a
Harpreet <ksfibri@gmail.com>
2026-07-29 10:52:28 -0500

    Add Categorypage with Expenses

---
+++ financetrackerAPI/Controllers/CategoryPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace financetrackerAPI.Controllers
+{
+    public class CategoryPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Category/Index.cshtml");
+        }
+    }
+}
