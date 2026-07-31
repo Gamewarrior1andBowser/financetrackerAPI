76a93682ecfed1c3b97756569efb21d306866bee
Emmanuel Olannye <eeolannye@gmail.com>
2026-07-31 08:53:43 -0500

    feat: Add UI for category folder

--- financetrackerAPI/Controllers/CategoryPageController.cs
+++ financetrackerAPI/Controllers/CategoryPageController.cs
@@ -8,5 +8,20 @@ namespace financetrackerAPI.Controllers
         {
             return View("~/Views/Category/Index.cshtml");
         }
+
+        public IActionResult Create()
+        {
+            return View("~/Views/Category/Create.cshtml");
+        }
+
+        public IActionResult Edit()
+        {
+            return View("~/Views/Category/Edit.cshtml");
+        }
+
+        //public IActionResult Details()
+        //{
+        //    return View("~/Views/Category/Details.cshtml");
+        //}
     }
 }
