e0619a3e5829b27c4a341e82567fab87f3dca6f4
Harpreet <ksfibri@gmail.com>
2026-08-07 11:32:42 -0500

    Error to show dummy data

--- financetrackerAPI/Controllers/HomeController.cs
+++ financetrackerAPI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace financetrackerAPI.Controllers
 {
@@ -6,6 +7,26 @@ namespace financetrackerAPI.Controllers
     {
         public IActionResult Index()
         {
+            string filePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "demo-data.json"
+            );
+
+
+            if (System.IO.File.Exists(filePath))
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+
+
+                ViewBag.DemoData = JsonSerializer.Deserialize<JsonElement>(json);
+            }
+            else
+            {
+                ViewBag.DemoData = null;
+            }
+
+
             return View();
         }
     }
