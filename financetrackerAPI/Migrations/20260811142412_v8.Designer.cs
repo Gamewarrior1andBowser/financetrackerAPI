0429a8f67fe1202b2ceb022adbb3fae577933df6
Emmanuel Olannye <eeolannye@gmail.com>
2026-08-11 09:32:37 -0500

    chore: pull from master into new branch.

--- financetrackerAPI/Migrations/20260811142412_v8.Designer.cs
+++ financetrackerAPI/Migrations/20260811142412_v8.Designer.cs
@@ -12,8 +12,8 @@ using financetrackerAPI.Data;
 namespace financetrackerAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260807151723_v7")]
-    partial class v7
+    [Migration("20260811142412_v8")]
+    partial class v8
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -33,22 +33,20 @@ namespace financetrackerAPI.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("budgetID"));
 
-                    b.Property<DateTime>("Date")
+                    b.Property<DateTime>("date")
                         .HasColumnType("datetime2");
 
-                    b.Property<int>("Limit")
-                        .HasColumnType("int")
-                        .HasColumnName("limits");
-
-                    b.Property<int>("categoryID")
+                    b.Property<int>("limits")
                         .HasColumnType("int");
 
                     b.Property<int>("userID")
                         .HasColumnType("int");
 
-                    b.HasKey("budgetID");
+                    b.Property<string>("username")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
-                    b.HasIndex("categoryID");
+                    b.HasKey("budgetID");
 
                     b.ToTable("Budget");
                 });
@@ -85,15 +83,15 @@ namespace financetrackerAPI.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("transactionsID"));
 
-                    b.Property<decimal>("Amount")
+                    b.Property<int>("amount")
                         .HasPrecision(10, 2)
-                        .HasColumnType("decimal(10,2)");
-
-                    b.Property<DateTime>("Date")
-                        .HasColumnType("datetime2");
+                        .HasColumnType("int");
 
                     b.Property<int>("categoryID")
                         .HasColumnType("int");
+
+                    b.Property<DateTime>("date")
+                        .HasColumnType("datetime2");
 
                     b.Property<int>("userID")
                         .HasColumnType("int");
@@ -133,31 +131,15 @@ namespace financetrackerAPI.Migrations
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("financetrackerAPI.Models.Budget", b =>
+            modelBuilder.Entity("financetrackerAPI.Models.Transaction", b =>
                 {
-                    b.HasOne("financetrackerAPI.Models.Category", "categories")
+                    b.HasOne("financetrackerAPI.Models.Category", "Category")
                         .WithMany()
                         .HasForeignKey("categoryID")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.Navigation("categories");
-                });
-
-            modelBuilder.Entity("financetrackerAPI.Models.Transaction", b =>
-                {
-                    b.HasOne("financetrackerAPI.Models.Category", "Category")
-                        .WithMany("Transactions")
-                        .HasForeignKey("categoryID")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
                     b.Navigation("Category");
-                });
-
-            modelBuilder.Entity("financetrackerAPI.Models.Category", b =>
-                {
-                    b.Navigation("Transactions");
                 });
 #pragma warning restore 612, 618
         }
