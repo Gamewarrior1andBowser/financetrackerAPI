3043f6203cc47dc4f10ae41b4059c9aacc476434
David Nwachukwu <davidike444@gmail.com>
2026-08-14 08:37:40 -0500

    categories cascade transactions deletion

---
+++ financetrackerAPI/Migrations/20260814131332_FixTransactionCategoryRelationship.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace financetrackerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTransactionCategoryRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
